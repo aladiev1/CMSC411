34871ac07077779f0d47c238b94cfb1bd48eb13e
Kevin Miller <KevM1227@gmail.com>
2017-12-02 17:22:58 -0500

    Added generator for atanh table. Also added output to google doc

--- generateatantable.py
+++ generateatantable.py
@@ -1,9 +1,20 @@
 import math
 import struct
 bin(struct.unpack('!i',struct.pack('!f',1.0))[0])
-def genTable():
+
+def genATanTable():
+	print("Atan Table-----------------------------")
 	for i in range(0, 32):
 		print(bin(struct.unpack('!i',struct.pack('!f',math.atan(1/(2**i))))[0]))
+	print("---------------------------------------")
 
+def genATanhTable():
+	print("Atan hyperbolic Table------------------")
+	for i in range(1, 32):
+		print(bin(struct.unpack('!i',struct.pack('!f',math.log((1 + 1/(2**i)) / (1 - 1/(2**i)))))[0]))
+	print("---------------------------------------")
+
+		
 if __name__ == "__main__":
-	genTable()
+	genATanTable()
+	genATanhTable()
