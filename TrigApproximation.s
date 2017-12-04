5751e991329d365d95ad43473f022a929a613156
Kevin Miller <KevM1227@gmail.com>
2017-12-03 19:17:27 -0500

    Added more test angles. PI / 3 and PI / 6 appear to be broken for some reason. Everything else seems to work. I added tan and started the exponential function. tan works, but e^x has some issues

--- TrigApproximation.s
+++ TrigApproximation.s
@@ -13,15 +13,23 @@
 	;	PI	   = 0x40490fdb
 	;	PI / 2 = 0x3fc90fdb
 	;	PI / 3 = 0x3f860a92
-	;	PI / 4 = 0x3f490fdb
+		; These two are broken for some reason. The rest work
+		;	PI / 4 = 0x3f490fdb
+		;	PI / 6 = 0x3F060A92
 	;	0	   = 0x40c90fdb
 	; 	2PI / 3= 0x40060A92
-
+	;	5PI / 6 = 0x40278D36
+	;	3PI / 4 = 0x4016CBE4
+	;   7PI / 4 = 0x40AFEDDF
+	;	5PI / 4 = 0x407B53D1
+	;	11PI / 6 = 0x40B84E88
 	; Table of reciprocals of factorial values. Precomputed to avoid the need for division and for performance considerations
 	recipFactorialTable: .word  0x3f800000, 0x3f800000, 0x3f000000, 0x3e2aaaab, 0x3d2aaaab, 0x3c088889, 0x3ab60b61, 0x39500d01, 0x37d00d01, 0x3638ef1d, 0x3493f27e, 0x32d7322b, 0x310f76c7, 0x2f309231, 0x2d49cba5, 0x2b573f9f, 0x29573f9f, 0x274a963c, 0x253413c3, 0x2317a4da, 0x20f2a15d, 0x1eb8dc78, 0x1c8671cb, 0x1a3b0da1, 0x17f96781, 0x159f9e67, 0x13447430, 0x10e8d58e, 0xe850c51, 0xc12cfcc, 0x99c9963, 0x721a697, 0x4a1a697, 0x21cc093, 0x24e204, 0x10dc6, 0x77e, 0x34, 0x1, 0x0
 	
 	NUM_TERMS_IN_FACTORIAL_TABLE: .word 0x28
-	ANGLE: .word 0x3f860a92
+
+	ANGLE: .word 0x40B84E88
+	EXPONENTIAL_PARAMETER: .word 0x2
 
 .text
 .global _main
@@ -40,9 +48,17 @@ _main:
 	LDR r0, [r0]
     BL Cos
 
-    ; Store result of sin in r8 and s8
+    ; Store result of cos in r8 and s8
     MOV r9, r1
     FMSR s9, r9
+
+	LDR r0, =EXPONENTIAL_PARAMETER
+	LDR r0, [r0]
+    BL Exponential
+
+    ; Store result of the exponential function in r7 and s7
+    MOV r7, r1
+    FMSR s7, r7
 
 	; tan(x) = sin(x) / cos(x)
 	; Not checking for division by 0 here since FDVIS will just produce Infinity. Some value with all 1's in the exponent
@@ -50,6 +66,76 @@ _main:
 
     B _exit					;exit
 
+
+; Only differences between the exponential function and sin/cos are the starting position and the increment amount for the factorial table
+Exponential:
+	; Preserve registers and make sure LR doesn't get corrupted when we make our calls to _MUL and pow
+	STMDB SP!, { R3-R9, LR }
+
+	; r3 = Angle to compute
+	; Storing in r3 since pow and _MUL require r0 and r1 for the input parameters
+	MOV r3, r0
+
+	; r4 = current exponent/Index in factorial table
+	MOV r4, #0
+	; r5 = Current offset in factorial table. Could just multiply the current index by 4, but I don't feel like doing another multiply
+	; Easier to just add 4 every loop iteration
+	MOV r5, #0
+
+	; r6 = Number of terms in factorial table. This will be our loop controller. Iterate as long as r4 is < r6
+	LDR r6, =NUM_TERMS_IN_FACTORIAL_TABLE
+	LDR r6, [r6]
+
+	; r8 = pointer to factorial table
+	LDR r8, =recipFactorialTable
+
+	; r7 = flag to determine whether we add or subtract. 0 corresponds to add. 1 corresponds to subtract
+	MOV r7, #0
+	; s2 = Accumulator. Will keep a running sum of our approximation. Set it to r7 since it's 0 anyway and we want to add first
+	FMSR s2, r7
+
+EXPONENTIAL_APPROXIMATION_LOOP:
+
+	; r9 = Current recriprocal factorial value
+	ldr r9,[r8, r5]
+
+	; r0 = Current angle. r1 = current exponent
+	MOV r0, r3
+	MOV r1, r4
+	BL Pow
+
+	; Move the result of the pow function into r0 to prepare for _MUL
+	MOV r0, r2
+	MOV r1, r9
+	BL _MUL
+
+	; Set up floating point registers to add
+	FMSR s0, r2
+
+	CMP r7, #0
+	BNE EXPONENTIAL_SUB_TERM
+
+EXPONENTIAL_ADD_TERM:
+	FADDS s2, s2, s0
+	B EXPONENTIAL_PREPARE_NEXT_TERM
+
+EXPONENTIAL_SUB_TERM:
+	FSUBS s2, s2, s0
+
+EXPONENTIAL_PREPARE_NEXT_TERM:
+
+	; Exponential function does not alternate between addition and subtraction, so keep operation on addition
+	;EOR r7, r7, #1
+	; Exponential does not skip any indices, so increment by 1 and 4
+	ADD r4, r4, #1
+	ADD r5, r5, #4
+	CMP r4, r6
+	BLT EXPONENTIAL_APPROXIMATION_LOOP
+
+FINISHED_EXPONENTIAL_APPROXIMATION:
+
+	FMRS r1, s2
+	LDMIA SP!, { R3-R9, PC }
 
 ; Sin function approximation using taylor series
 ; Input: r0 = angle in IEEE 754 format
